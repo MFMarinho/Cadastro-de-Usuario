6be50255eb406ab1d48fac566b33003c49bf6660
MFMarinho <felixmarinho.matheus@gmail.com>
2024-10-01 13:19:08 -0300

    first commit

---
+++ usuarios/views.py
@@ -0,0 +1,45 @@
+from django.http.response import HttpResponse
+from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as login_django
+from django.contrib.auth.decorators import login_required
+
+
+def cadastro(request):
+    if request.method == "GET":
+        return render(request, 'cadastro.html')
+    else:
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+
+        user = User.objects.filter(username=username).first()
+
+        if user:
+            return HttpResponse('Já existe um usuário com esse nome.')
+        
+        user = User.objects.create_user(username=username, email=email, password=senha)
+        user.save()
+
+        return HttpResponse('Usuário cadastrado com sucesso!')
+
+def login(request):
+    if request.method == "GET":
+        return render(request, 'login.html')
+    else:
+        username = request.POST.get('username')
+        senha = request.POST.get('senha')
+
+        user = authenticate(username=username, password=senha)
+
+        if user:
+            login_django(request, user)
+
+            return HttpResponse('Autenticado')
+        else:
+            return HttpResponse('Email ou senha inválido')
+        
+@login_required(login_url="/auth/login")
+def plataforma(request):
+    return HttpResponse('Só tem acesso a essa parte logado')
